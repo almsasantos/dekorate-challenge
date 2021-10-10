3596c6cfb6baf193620852f95b13e7a4b94d14df
user1 <analu.martinsdossantos@gruposantander.com>
2021-10-10 11:29:10 +0000

    added tests

--- src/main/java/com/santander/games/challenges/dekorate/DekorateChallengeApplication.java
+++ src/main/java/com/santander/games/challenges/dekorate/DekorateChallengeApplication.java
@@ -3,10 +3,13 @@ package com.santander.games.challenges.dekorate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.dekorate.openshift.annotation.OpenshiftApplication;
+
+@OpenshiftApplication(name = "challenge-2")
 @SpringBootApplication
 public class DekorateChallengeApplication {
 
-	public static void main(String[] args) {
+        public static void main(String[] args) {
 		SpringApplication.run(DekorateChallengeApplication.class, args);
 	}
 
--- src/test/java/com/santander/games/challenges/dekorate/DekorateChallengeApplicationTests.java
+++ src/test/java/com/santander/games/challenges/dekorate/DekorateChallengeApplicationTests.java
@@ -7,8 +7,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 
 import static io.restassured.RestAssured.given;
+import io.dekorate.utils.Serialization;
+import io.fabric8.kubernetes.api.model.HasMetadata;
+import io.fabric8.kubernetes.api.model.KubernetesList;
+import io.fabric8.openshift.api.model.BuildConfig;
+import io.fabric8.openshift.api.model.DeploymentConfig;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import static org.hamcrest.CoreMatchers.is;
+
+import java.util.Optional;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DekorateChallengeApplicationTests {
@@ -30,5 +39,21 @@ class DekorateChallengeApplicationTests {
 				.body(is("Hello world"));
 	}
 
+    @Test
+    public void shouldHaveMatchingOutputImageAndTrigger() {
+        KubernetesList list = Serialization.unmarshalAsList(getClass().getClassLoader().getResourceAsStream("META-INF/dekorate/openshift.yml"));
+        assertNotNull(list);
+        DeploymentConfig d = findFirst(list, DeploymentConfig.class).orElseThrow(() -> new IllegalStateException());
+        BuildConfig b = findFirst(list, BuildConfig.class).orElseThrow(() -> new IllegalStateException());
+        assertNotNull(d);
+        assertNotNull(b);
+        assertTrue(d.getSpec().getTriggers().stream().filter(t -> t.getImageChangeParams().getFrom().getName().equals(b.getSpec().getOutput().getTo().getName())).findFirst().isPresent());
+    }
+
+    <T extends HasMetadata> Optional<T> findFirst(KubernetesList list, Class<T> t) {
+        return (Optional<T>) list.getItems().stream()
+        .filter(i -> t.isInstance(i))
+        .findFirst();
+    }
 
 }
